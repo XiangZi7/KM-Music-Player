0149a04ba81af6f24168d415794b4094aa47883e
XiangZi7 <793923048@qq.com>
2023-11-10 15:44:19 +0800

    修改search名字

---
+++ src/pages/Searchs/components/single.jsx
@@ -0,0 +1,16 @@
+import MkTable from "@/components/Mk-Table/Mk-Table.jsx";
+
+
+export default function single({ data }) {
+  return (
+    <>
+      {/*歌曲*/}
+      {
+        <div className="content-section mt-10">
+          <div className="content-section-title">Album</div>
+          <MkTable data={data} style={{ maxHeight: "55vh" }} />
+        </div>
+      }
+    </>
+  );
+}
