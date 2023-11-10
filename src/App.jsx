ddaf638f135dfe69e1a22def39d0843234845891
XiangZi7 <793923048@qq.com>
2023-11-10 15:01:12 +0800

    修改css地址

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import DefaultLayouts from '@/layouts/index.jsx'
-
+import './styles/app.scss'
+import './styles/player.scss'
 function App() {
 
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import '@/styles/app.scss'
-import '@/styles/player.scss'
+
 
 // Redux
 import store, {persistor} from "./stores/index.js";
