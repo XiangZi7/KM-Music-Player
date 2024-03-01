154a8d782950362b983096d5cc05b053eff0efc0
XiangZi7 <793923048@qq.com>
2024-03-01 11:09:39 +0800

    更改接口

--- src/layouts/components/data.jsx
+++ src/layouts/components/data.jsx
@@ -1,16 +1,15 @@
-import {Film, Home, Layout, Music, PlayCircle, Thermometer, Trello} from 'react-feather';
+import { Film, Home, Layout, Music, PlayCircle, Thermometer, Trello } from 'react-feather';
 
 // 根据路由表生成对应的路由规则
 export const items = [{
     title: "Apps",
     children: [
-        {path: '/home', title: "Home", icon: <Home className="text-2xl"/>},
-        {path: '/ml', title: "Music library", icon: <Music className="text-2xl"/>},
-        {path: '/movie', title: "Movie", icon: <Film className="text-2xl"/>},
-        {path: '/mini', title: "Mini Player", icon: <Trello className="text-2xl"/>},
-        {path: '/chat', title: "AI", icon: <PlayCircle className="text-2xl"/>},
-        {path: '/theme', title: "Theme", icon: <Layout className="text-2xl"/>},
-        {path: '/test', title: "Test", icon: <Thermometer className="text-2xl"/>},
+        { path: '/home', title: "Home", icon: <Home className="text-2xl" /> },
+        { path: '/ml', title: "Music library", icon: <Music className="text-2xl" /> },
+        { path: '/movie', title: "Movie", icon: <Film className="text-2xl" /> },
+        { path: '/mini', title: "Mini Player", icon: <Trello className="text-2xl" /> },
+        { path: '/theme', title: "Theme", icon: <Layout className="text-2xl" /> },
+        { path: '/test', title: "Test", icon: <Thermometer className="text-2xl" /> },
     ],
 },
 ]
